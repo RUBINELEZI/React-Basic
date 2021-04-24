0248e586beee6a149d87b9e29a8b1a611cf3aac1
Rubin <rubinelezi0@gmail.com>
2021-04-24 16:57:52 +0200

    switch categories

--- src/components/SingleCategory/SingleCategory.jsx
+++ src/components/SingleCategory/SingleCategory.jsx
@@ -3,14 +3,17 @@ import './singleCategory.css'
 import { useSelector, useDispatch} from "react-redux";
 import {categoryAction} from "../../redux/actions/categoryAction";
 import {userAction} from "../../redux/actions/userAction";
+import {btnClickAction} from "../../redux/actions/btnClickAction";
 const SingleCategory = ({title}) => {
 
     const dispatch = useDispatch()
+    const btnState = useSelector(state => state.btn)
     let handleClick = (e) =>{
         // fetch("http://mjeket.herokuapp.com/doctors/api/alldoctors/" + e.target.innerText)
         //     .then(response => response.json())
         //     .then(data =>  )
         dispatch(categoryAction(e.target.innerText))
+        dispatch(btnClickAction(btnState))
     }
 
     return (
